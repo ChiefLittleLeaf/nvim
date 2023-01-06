60a967107d604eca1e4cbc93435b34ed1ebbd6da
ChiefLittleLeaf <haddockmason@gmail.com>
2023-01-06 13:18:15 -0600

    My nvim setup using lua

---
+++ lua/chieflittleleaf/remap.lua
@@ -0,0 +1,50 @@
+-- sets the leader key to a space
+vim.g.mapleader = " "
+
+-- allows you to jump back into the netrw view from a file
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+-- remap for quick saving of files
+vim.keymap.set("n", "<leader>w", vim.cmd.write)
+
+-- allows in visual mode to move the highlighted using J or K
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+vim.keymap.set("n", "Y", "yg$")
+vim.keymap.set("n", "J", "mzj`z")
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "nzzzv")
+
+vim.keymap.set("x", "<leader>p", "\"_dP")
+
+-- copy buffer seperation stuff
+vim.keymap.set("n", "<leader>y", "\"+y")
+vim.keymap.set("v", "<leader>y", "\"+y")
+vim.keymap.set("n", "<leader>Y", "\"+Y")
+
+-- delete buffer stuff
+vim.keymap.set("n", "<leader>d", "\"_d")
+vim.keymap.set("n", "<leader>d", "\"_d")
+
+-- i do not use ; often so remapped to be just :
+vim.keymap.set("n", ";", ":")
+
+-- remapped for quick esc functionality this feels more normal
+vim.keymap.set("i", "<C-k>", "<Esc>")
+
+vim.keymap.set("n", "Q", "<nop>")
+vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
+vim.keymap.set("n", "<leader>f", function()
+    vim.lsp.buf.format()
+end)
+
+vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
+vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
+vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
+vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
+
+vim.keymap.set("n", "<leader>s", ":%s/\\<<C-r><C-w>\\>/<C-r><C-w>/gI<Left><Left><Left>")
+vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
