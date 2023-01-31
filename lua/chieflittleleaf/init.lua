4362fe8bcf51fcf1d718bb7f95ea05d869217f68
ChiefLittleLeaf <haddockmason@gmail.com>
2023-01-30 18:17:22 -0600

    Update

--- lua/chieflittleleaf/init.lua
+++ lua/chieflittleleaf/init.lua
@@ -32,7 +32,3 @@ autocmd({ "BufWritePre" }, {
 vim.g.netrw_browse_split = 0
 vim.g.netrw_banner = 0
 vim.g.netrw_winsize = 25
-
-vim.cmd [[autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()]]
--- or
-vim.cmd [[autocmd BufWritePre * lua vim.lsp.buf.formatting_sync()]]
